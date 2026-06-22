8a5ed90cf780a870f6a62896693c57611de1fb2f
Ashish Rajput <ashishrajput000261@gmail.com>
2026-06-23 04:10:51 +0530

    Day 18: add delete contact API

--- Backend/backend/src/main/java/com/msteleinfra/backend/controller/ContactController.java
+++ Backend/backend/src/main/java/com/msteleinfra/backend/controller/ContactController.java
@@ -1,10 +1,18 @@
 package com.msteleinfra.backend.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.msteleinfra.backend.entity.Contact;
 import com.msteleinfra.backend.service.ContactService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/contact")
@@ -25,5 +33,11 @@ public class ContactController {
     @GetMapping
     public List<Contact> getAllContacts() {
         return contactService.getAllContacts();
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteContact(@PathVariable Long id) {
+        contactService.deleteContact(id);
+        return "Contact inquiry deleted successfully";
     }
 }
